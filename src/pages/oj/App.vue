bd5a605ef8d2e5dc18221188a3027ae45c65e64b
wooyoung85 <cerealtigerpower@gmail.com>
2019-06-27 19:30:07 +0900

    update

--- src/pages/oj/App.vue
+++ src/pages/oj/App.vue
@@ -7,7 +7,7 @@
       </transition>
       <div class="footer">
         <p v-html="website.website_footer"></p>
-        <p>Powered by <a href="https://github.com/QingdaoU/OnlineJudge">OnlineJudge</a>
+        <p>Powered by <a href="https://wooyoung85.tistory.com">서우영</a>
           <span v-if="version">&nbsp; Version: {{ version }}</span>
         </p>
       </div>
